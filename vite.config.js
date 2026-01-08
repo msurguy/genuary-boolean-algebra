4d86fbe905a372c7328d61789ca15fe953044bb3
Maksim <msurguy@gmail.com>
2026-01-08 00:47:49 -0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src",
+  base: "/genuary-boolean-algebra/",
   build: {
     outDir: "../docs",
     emptyOutDir: true,
